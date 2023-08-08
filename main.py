7a387a4676ee6acb78cb032673ee04fcb19b374b
Sanjaypranav <sanjaypriya195@gmail.com>
2023-08-08 19:08:12 +0530

    updated tts

--- main.py
+++ main.py
@@ -4,14 +4,6 @@ from scipy.io.wavfile import write
 import assemblyai as aai
 
 
-# def get_text(path: str) -> str:
-#     with open("keys.txt") as f:
-#       key = f.read()
-#     aai.settings.api_key = key
-#     transcriber = aai.Transcriber()
-#     transcript = transcriber.transcribe(path)
-#     return transcript.text
-
 def get_text(path: str) -> str:
     return "Hello World"
 
