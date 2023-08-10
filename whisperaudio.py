d8279e356962854a91c85d12bf4539434f72d826
Sanjaypranav <sanjaypriya195@gmail.com>
2023-08-10 12:10:23 +0530

    commented

--- whisperaudio.py
+++ whisperaudio.py
@@ -4,8 +4,8 @@ import requests
 with open("keys.txt", "r") as f:
     key = f.read()
 
-# API_URL = "https://p0oi0gtn7iyhoioj.us-east-1.aws.endpoints.huggingface.cloud/"
-API_URL = "https://z4dl3t7upduo9inw.us-east-1.aws.endpoints.huggingface.cloud/"
+API_URL = "https://p0oi0gtn7iyhoioj.us-east-1.aws.endpoints.huggingface.cloud/" #--> english
+# API_URL = "https://z4dl3t7upduo9inw.us-east-1.aws.endpoints.huggingface.cloud/" --> tamil
 API_TOKEN = key
 headers = {"Authorization": f"Bearer {API_TOKEN}", "Content-Type": "audio/mpeg"}
 
