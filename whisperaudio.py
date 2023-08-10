7b4931b547b7fb12fa6a9935b3aec3a62b988d41
Sanjaypranav <sanjaypriya195@gmail.com>
2023-08-10 10:24:46 +0530

    whisper updated

--- whisperaudio.py
+++ whisperaudio.py
@@ -4,9 +4,9 @@ import requests
 with open("keys.txt", "r") as f:
     key = f.read()
 
-API_URL = "https://api-inference.huggingface.co/models/vasista22/whisper-tamil-medium"
+API_URL = "https://z4dl3t7upduo9inw.us-east-1.aws.endpoints.huggingface.cloud/"
 API_TOKEN = key
-headers = {"Authorization": f"Bearer {API_TOKEN}"}
+headers = {"Authorization": f"Bearer {API_TOKEN}", "Content-Type": "audio/mpeg"}
 
 def get_text(file: str) -> str:
     return requests.post(API_URL, headers=headers, data = file).json()
