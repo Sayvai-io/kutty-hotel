829b3369a843b533999c25814d772c72540d2b9f
Sanjaypranav <sanjaypriya195@gmail.com>
2023-08-10 10:45:14 +0530

    changed to english stt

--- whisperaudio.py
+++ whisperaudio.py
@@ -4,7 +4,7 @@ import requests
 with open("keys.txt", "r") as f:
     key = f.read()
 
-API_URL = "https://z4dl3t7upduo9inw.us-east-1.aws.endpoints.huggingface.cloud/"
+API_URL = "https://p0oi0gtn7iyhoioj.us-east-1.aws.endpoints.huggingface.cloud/"
 API_TOKEN = key
 headers = {"Authorization": f"Bearer {API_TOKEN}", "Content-Type": "audio/mpeg"}
 
